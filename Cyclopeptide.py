4b19cffb60d48176341c24892eecf3f631a848ef
Ahmed Nasser <ahmed20191701016@cis.asu.edu.eg>
2022-04-05 05:13:53 +0200

    Update Cyclopeptide.py

--- Cyclopeptide.py
+++ Cyclopeptide.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr  2 21:50:05 2022
+Updated on Tue Apr  5 04:59:05 2022
 
 @author: Ahmed Nasser
 """
@@ -23,50 +24,72 @@ except IOError:
     print("File (%s) doesn't exist" % fileName)
 
 
-
-#Extracts all the (1-mer) of the Peptide
+# Extracts all the (1-mer) of the Peptide
 def Initial_List(spect):
     listInit = []
-    for i in range(len(spect)):
+    for i in range(len(spect)):  # 21
         if spect[i] in weightsDict.keys():
             listInit.append(weightsDict[spect[i]])
+
+    print("listInit: %s\n" % listInit)
     return listInit
 
 
-
-#Calculate the (k-mers) Scores
+# Calculate the (k-mers) Scores
 def Linear_Spectrum(peptide):
-    sublist=[peptide]
     for i in range(len(peptide)):
-        for j in range(len(peptide)):
-            if (i+j) < len(peptide):
-                 sublist.append(peptide[j:(i+j)+1])
-            else:
-                sublist.append(peptide[j:len(peptide)]+peptide[0:(i+j)-len(peptide)])
-    return sublist
+        for j in range(i, len(peptide)):
+            subList.append(peptide[i:j+1])
+
+    print(subList)
+    return subList
 
 
+def IsConsistent(fragSpect):
+    total = 0
+    weightsList = []
+    wl = []
 
-def IsConsistent(z):
-    pass
+    for i in range(len(fragSpect)):
+        for j in weightsDict:
+            if fragSpect[i] == weightsDict[j]:
+                weightsList.append(j)
+                wl.append(j)
 
+    wlLength = len(weightsList)
+
+    for i in range(wlLength):
+        total += weightsList[i]
+        for j in range(i, wlLength):
+            tmp = weightsList[i] + weightsList[j]
+            wl.append(tmp)
+
+    wl.append(total)
+
+    print(wl)
+    return
 
 
 def Extraction(initList):
-    z = list(Initial_List(initList))
+    y = Linear_Spectrum(Initial_List(initList))
 
-    for i in range(2, len(z)):
-        z = Linear_Spectrum(z)
-        IsConsistent(z)
-    return z
-
+    for i in range(len(y)):
+        IsConsistent(y[i])
+    return
 
 
 
 
 
+if __name__ == "__main__":
+    '''TestCase -> Theoritcal Spectrum:
+                0 97 97 99 101 103 196 198 200 202 295 297 299 299 301 394 396 398 400 400 497
+    '''
 
-#Main
-initList = list(input("Enter Your CycloPeptide Seq: ").split(" ")).sort()
+    subList = []
+    initListSpect = [int(x) for x in (
+        list(input("Enter Your CycloPeptide Seq: ").split(' '))
+    )]
+    initListSpect.sort()
 
-Extraction(initList)
+    Extraction(initListSpect)
